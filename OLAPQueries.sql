4f04caceb700f2c7f8b079f6b8a04131e78c0d0b
Chatura Ahangama <chaturadroid@gmail.com>
2019-02-05 22:33:06 -0800

    added the OLAP queries

---
+++ OLAPQueries.sql
@@ -0,0 +1,18 @@
+
+-- 1. Where the book is available and what row
+SELECT * FROM groupprojectolap.dimbooks where available = 1 ;
+
+-- 2. What books are not available and not shelved
+SELECT * FROM groupprojectolap.dimbooks where available is null ;
+
+-- 3. Book witht the greatest cost and not available to acquire top 3 books
+SELECT * FROM groupprojectolap.dimbooks where available is null  ORDER BY bookCost DESC
+LIMIT 3;
+
+-- 4. Havent had any past due but live in california
+SELECT * FROM groupprojectolap.dimborrowers where booksPastDue = 'N' and borrowerCity like 'CA';
+
+-- 5. find all the empty isles in all locations //able to fill
+select * from groupprojectolap.dimlocations where contents is null;
+
+
